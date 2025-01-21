3b8aa2d20dc4bcbf3a58152cba0ac999ad836706
Arturo <arturcas2@hotmail.com>
2025-01-21 00:06:59 -0600

    Adding render button and modal in the project

---
+++ src/users/presentation/render-modal/render-modal.js
@@ -0,0 +1,17 @@
+import modalHTML from "./render-modal.html?raw";
+let modal;
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const renderModal = (element) => {
+    if (modal) return;
+
+    modal = document.createElement('div');
+    modal.innerHTML = modalHTML;
+    modal.className = 'modal-container hide-modal';
+
+    element.append(modal);
+}
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,8 @@
-import { renderButtons } from "./presentation/render-buttons/render-buttons";
-import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store"
+import { renderTable } from "./presentation/render-table/render-table";
+import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderAddButton } from "./presentation/render-add-button/render-add-button";
+import { renderModal } from "./presentation/render-modal/render-modal";
 
 /**
  * 
@@ -13,4 +15,6 @@ export const UsersApp = async(element) => {
 
     renderTable(element);
     renderButtons(element);
+    renderAddButton(element);
+    renderModal(element);
 }
